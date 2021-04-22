5f1568b83c32c75651590b00d8098f5be619b795
Sophia <soapsstone@gmail.com>
2021-04-22 15:01:18 -0400

    dinner-party

---
+++ dinner-party/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dinner_party
+{
+    class Program
+    {
+
+        public class Guest
+        {
+            public string Name {get; set;}
+            public string Occupation {get; set;}
+            public string Bio {get; set;}
+        }
+
+        public class tableList
+        {
+            public string tableOne {get; set;}
+            public string tableTwo {get; set;}
+        }
+        public static void Main() 
+        {
+            List<Guest> guests = new List<Guest>() 
+            {
+                new Guest(){ Name="Marilyn Monroe", Occupation="entertainer", Bio="(1926 - 1962) American actress, singer, model"},
+                new Guest(){ Name="Abraham Lincoln", Occupation="politician", Bio="(1809 - 1865) US President during American civil war"},
+                new Guest(){ Name="Martin Luther King", Occupation="activist", Bio="(1929 - 1968)  American civil rights campaigner"},
+                new Guest(){ Name="Rosa Parks", Occupation="activist", Bio="(1913 - 2005)  American civil rights activist"},
+                new Guest(){ Name="Peter Sellers", Occupation="entertainer", Bio="(1925 - 1980) British actor and comedian"},
+                new Guest(){ Name="Alan Turing", Occupation="computer scientist", Bio="(1912 - 1954) - British computing pioneer, Turing machine, algorithms, cryptology, computer architecture, saved the world"},
+                new Guest(){ Name="Admiral Grace Hopper", Occupation="computer scientist", Bio="(1906–1992) - developed early compilers: FLOW-Matic, COBOL; worked on UNIVAC; gave speeches on computer history, where she gave out nano-seconds"},
+                new Guest(){ Name="Indira Gandhi", Occupation="politician", Bio="(1917 - 1984) Prime Minister of India 1966 - 1977"},
+            };
+
+            List<Guest> guestAtTable1 = new List<Guest>();
+            List<Guest> guestAtTable2 = new List<Guest>();
+
+            //need object to put each table in then a condition similar to the one in js
+            // foreach (Guest g in guests)
+
+            // Console.WriteLine($"{g.Name} {g.Occupation} {g.Bio}");
+
+              foreach (Guest g in guests) {
+                List<string> table1Occupations = guestAtTable1.Select(g => g.Occupation).ToList();
+                if (table1Occupations.Contains(g.Occupation)) {
+                     guestAtTable2.Add(g);
+                } else {
+                    guestAtTable1.Add(g);
+                }
+
+            }
+
+            Console.WriteLine("Table 1");
+                foreach (Guest guest in guestAtTable1)
+                {
+                    Console.WriteLine($"{guest.Name} {guest.Occupation} {guest.Bio}");
+                }
+            
+            Console.WriteLine();
+            Console.WriteLine("Table 2");
+                foreach (Guest guest in guestAtTable2)
+                {
+                    Console.WriteLine($"{guest.Name} {guest.Occupation} {guest.Bio}");
+                }
+    }
+}
+}
---
+++ insulter/csharp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        List<string> insults = new List<string>() 
+            {
+                "You look like what morning breath smells like.",
+                "If you tried to give me cpr I would probably throw myself back under water",
+                "I am not a fan of you",
+                "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
+                "I'd rather walk than be on the same plane as you",
+                "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
+                "Do you have to be so...like that?",
+                "If you were a vegetable, you'd be a cabbitch",
+            };
+
+            // Phase one: Print on random insult
+            // Step one: print a non-random insult, but just one
+            // Console.WriteLine(insults[7]);
+            // Console.WriteLine(insults[1]);
+            // int randomNum = new Random().Next(0,7);
+            // Console.WriteLine(randomNum);
+            // Console.WriteLine(insults[randomNum])
+
+            //Phase two: Print 3 random insults 
+            // int randomNumOne = new Random().Next(0,7);
+            // Console.WriteLine(randomNumOne);
+            // Console.WriteLine(insults[randomNumOne]);
+
+            // int randomNumTwo = new Random().Next(0,7);
+            // Console.WriteLine(randomNumTwo);
+            // Console.WriteLine(insults[randomNumTwo]);
+
+            // int randomNumThree = new Random().Next(0,7);
+            // Console.WriteLine(randomNumThree);
+            // Console.WriteLine(insults[randomNumThree]);
+
+            //Phase three: prevent duplicates
+            
+            
+            List<int> randomNumbers = new List<int>(){};
+
+            while (randomNumbers.Count < 3){
+                int singleRandomNumOne = new Random().Next(0,6);
+
+                if (!randomNumbers.Contains(singleRandomNum)) {
+                    randomNumbers.Add(singleRandomNum);
+                }
+            }
+            //randomNumbers.ForEach( numb => Console.WriteLine(num));
+            foreach (int randomNumber in randomNumbers) {
+
+            }
+
+        }
+    }
+}
